73f8b6e496b3aeca27796f9e55b7cd1eadb2a772
Jonathan Cobos <jonathan.cobos@outlook.es>
2023-06-03 20:23:58 -0600

    Added add department and add role, thought add role its not working yet

--- develop/db/schema.sql
+++ develop/db/schema.sql
@@ -12,7 +12,7 @@ CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     job_title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
-    department_id INT,
+    department_id INT ,
     FOREIGN KEY(department_id)
     REFERENCES department(id)
     ON DELETE SET NULL
--- develop/js/index.js
+++ develop/js/index.js
@@ -29,75 +29,120 @@ const db = mysql.createConnection(
     console.log(`Connected to the main_db database `)
 );
 
+
 const userInput = () => {
     inquirer
         .prompt([
             {
-                type:'list',
+                type: 'list',
                 message: 'What would you like to do?',
                 name: 'action',
                 choices: [
                     'View all departments', 'View all roles', 
-                    'View all employees', 'Add a department', 
-                    'Add a role', 'Add an employee', 
-                    'Update an employee'
-                ],
+                    'View all employees', 'Add a department',
+                    'Add a role'
+                    ],
             }
         ])
         .then((data) => {
-            
-            switch(data.action) {
-                case 'View all departments':
-                    db.query('select * from department', (err, results) => {
-                        console.table(results);
+            if(data.action === 'Add a department') {
+                inquirer
+                    .prompt([
+                        {
+                            type: 'input',
+                            message: 'What department would you like to add? ',
+                            name: 'dp_name'
+                        },
+                    ]).then((answer) => {
+                        const departmentName = answer.dp_name;
+                        db.query(`INSERT INTO department(department_name) VALUES ("${departmentName}");`, (err, results) => {
+                            if(err) {
+                                console.error(err);
+                            } else {
+                                console.log('\n');
+                                console.log(`${departmentName} department added succesfully!`);
+                                console.log('\n');
+                                userInput();
+                            }
+                        });
                     });
-                    break;
-            }
+                } else if(data.action === 'Add a role') {
+                    inquirer.prompt([
+                        {
+                            type: 'input',
+                            message: 'Type the name of the role: ',
+                            name: 'role_name'
+                        },
+                        {
+                            type: 'input',
+                            message: 'Type the role salary: ',
+                            name: 'role_salary'
+                        },
+                        {
+                            type: 'list',
+                            message: 'Select the role id: 1-Production, 2-Sales, 3-Marketing, 4-Finance, 5-IT, 6-HR',
+                            name: 'role_id',
+                            choices: [1, 2, 3, 4, 5, 6]
+                        },
+                    ]).then((answers) => {
+                        const roleName = answers.role_name;
+                        const roleSalary = answers.role_salary;
+                        const roleDepartment = answers.role_id;
 
-            userInput();
-        })
+                        db.query(`INSERT INTO role (job_title, salary, department_id) VALUES ("${roleName}", "${roleSalary}, ${roleDepartment}")`, 
+                        (err, results) => {
+                            if(err) {
+                                console.log(err);
+                            } else {
+                                console.log('Role addes succesfully!');
+                                userInput();
+                            }
+                        });
+                    })
+
+                } else {
+                    mysqlQuery(data.action);
+                }
+            
+        });
 };
 
-userInput();
-
-// const userInput = () => {
-//   inquirer
-//     .prompt([
-//         {
-//             type: 'list',
-//             message: 'What would you like to do?',
-//             name: 'activity',
-//             choices: [
-//                 'View all departments', 'View all roles', 
-//                 'View all employees', 'Add a department', 
-//                 'Add a role', 'Add an employee', 
-//                 'Update an employee'
-//             ],
-//         },
-
-//     ])
-//     .then((data) => {
-//         switch(data.activity) {
+const mysqlQuery = (data) => {
+    switch(data) {
+        case 'View all departments':
+            db.query('Select * from department', (err, results) => {
+                console.log("\n");
+                console.table(results);
+                console.log("\n\n\n");
+            });
+            break;
         
-//             case 'View all departments':
-//                 db.query('SELECT * FROM department', (err, results) => {
-//                 console.table(results);
-//                  });
-//                 break;
+        case 'View all roles':
+            db.query('select * from role JOIN department ON department.id = role.department_id', (err, results) => {
+                console.log("\n");
+                console.table(results);
+                console.log("\n\n\n");
+            });
+            break;
+
+        case 'View all employees':
+            db.query('select * from employee JOIN role ON role.id = employee.role_id', (err, results) => {
+                console.log("\n");
+                console.table(results);
+                console.log("\n\n\n");
+            });
+            break;
+        
+    }
     
-//             case 'View all roles':
-//                 db.query('select * from department JOIN role ON department.id = role.department_id', (err, results) =>  {
-//                     console.table(results);
-//                 });
-//                 break;
-//         }
-        
-//         userInput();
+    
+    
+    userInput();
+};
 
-//     });
-// }
- 
-// userInput();
+
+
+userInput();
 
 module.exports = userInput;
 
